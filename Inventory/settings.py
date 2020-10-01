4291ff020ebae6544f93edd4c4d337a0f53249fc
truonghh99 <truonghh@beloit.edu>
2020-10-01 08:24:28 -0700

    Merge search interface into view.html

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 mimetypes.add_type("text/css", ".css", True)
